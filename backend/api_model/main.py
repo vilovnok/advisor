ee883f79b92ac52f9637a2af21711f9ec71e34bf
vilovnok <vilovnok@gmail.com>
2024-12-04 22:42:07 +0300

    add vllm

--- backend/api_model/main.py
+++ backend/api_model/main.py
@@ -2,9 +2,34 @@ import uvicorn
 from vllm import LLM, SamplingParams
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from .schema import (ChatRequest, ChatResponse, ChatResponseChoice)
+# from .schema import (ChatRequest, ChatResponse, ChatResponseChoice)
 
 from huggingface_hub import login
+
+
+from typing import List, Dict, Optional
+from pydantic import BaseModel
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+class ChatRequest(BaseModel):
+    model: str
+    messages: List[Message]
+    max_tokens: int = 256
+    temperature: float = 0.7
+    top_p: float = 0.9
+
+class ChatResponseChoice(BaseModel):
+    message: Dict[str, str]
+
+class ChatResponse(BaseModel):
+    choices: List[ChatResponseChoice]
+
+
+
 
 model = "meta-llama/Llama-2-7b-hf"
 token='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'
