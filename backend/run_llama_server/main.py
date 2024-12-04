abcc73b858c882738cf4a10a8a40e96c8d7e4794
vilovnok <vilovnok@gmail.com>
2024-12-04 23:01:28 +0300

    add vllm

--- backend/run_llama_server/main.py
+++ backend/run_llama_server/main.py
@@ -9,7 +9,8 @@ login(token=token)
 command = [
     "python", "-m", "vllm.entrypoints.openai.api_server", 
     "--model", "meta-llama/Llama-2-7b-hf", 
-    "--gpu-memory-utilization", "0.85"
+    "--gpu-memory-utilization", "0.85",
+    "--port", "7986"
 ]
 
 subprocess.run(command)
