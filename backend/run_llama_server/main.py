cc16568addbe97ed3e96b93b0186b6e7af751419
vilovnok <vilovnok@gmail.com>
2024-12-04 22:54:54 +0300

    add vllm

---
+++ backend/run_llama_server/main.py
@@ -0,0 +1,15 @@
+import subprocess
+
+token = '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'
+
+from huggingface_hub import login
+
+login(token=token)
+
+command = [
+    "python", "-m", "vllm.entrypoints.openai.api_server", 
+    "--model", "meta-llama/Llama-2-7b-hf", 
+    "--gpu-memory-utilization", "0.85"
+]
+
+subprocess.run(command)
