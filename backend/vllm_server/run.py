522d3a8e522b529f4532771a04c2d8848d76bea7
vilovnok <vilovnok@gmail.com>
2024-12-06 13:50:16 +0300

    fix

--- backend/vllm_server/run.py
+++ backend/vllm_server/run.py
@@ -6,7 +6,7 @@ login(token=token)
 
 command = [
     "python", "-m", "vllm.entrypoints.openai.api_server", 
-    "--model", "msu-rcc-lair/RuadaptQwen2.5-32B-instruct-GGUF:Q3_K_M", 
+    "--model", "msu-rcc-lair/RuadaptQwen2.5-32B-instruct-GGUF_Q3_K_M", 
     "--gpu-memory-utilization", "0.85",
     "--port", "7986"
 ]
